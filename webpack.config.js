a36e1f2a7e795c5a8e4e9b91029877207598b144
Hasst <sergey1112345@gmail.com>
2020-11-05 15:19:49 +0200

    configured: css-loaders

--- webpack.config.js
+++ webpack.config.js
@@ -38,4 +38,16 @@ module.exports = {
       filename: "bundle.[hash].css",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
 };
