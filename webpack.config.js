9b6d4da96e5ecb17257721760685962086d63e53
Hasst <sergey1112345@gmail.com>
2020-11-05 14:53:57 +0200

    added: installed  plugins are configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
-const path = require('path');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: {
-        main: './index.js'
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: "./index.js",
+  },
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        }
+      ]
+    }),
+  ],
 };
